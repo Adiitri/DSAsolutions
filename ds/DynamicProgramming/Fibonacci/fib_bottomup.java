4cd46e15b1674b19d6a18604dbcd323d3aea06f5
Aditri Sharma <165659165+Adiitri@users.noreply.github.com>
2025-10-07 12:33:14 +0530

    Update fib_bottomup.java

--- ds/DynamicProgramming/Fibonacci/fib_bottomup.java
+++ ds/DynamicProgramming/Fibonacci/fib_bottomup.java
@@ -1,4 +1,7 @@
 // Bottom-up/Iterative/Tabulation method 
+// - First we used and array then we optimised it to using 3 variable for storing the last three numbers
+// - We are following a btm-up tree (logically) the root- tribo(n)  is at the bottom
+
 class Solution {
     public int tribonacci(int n) {
         if (n == 0) return 0;
